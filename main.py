1a4492aa79580c083d8940c0612538fa637a04d6
soumalla <soumallatarafder@gmail.com>
2024-01-15 00:47:50 +0530

    main and config file final updated due to silly mistake

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from red_wine_quality_prediction import logger
 from red_wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from red_wine_quality_prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from red_wine_quality_prediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from red_wine_quality_prediction.pipeline.stage_04_data_trainer import ModelTrainerTrainingPipeline
+from red_wine_quality_prediction.pipeline.stage_04_data_trainer import DataTranerTrainingPipeline
 from red_wine_quality_prediction.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
@@ -46,7 +46,7 @@ except Exception as e:
 STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   pipeline_stage_04 = ModelTrainerTrainingPipeline()
+   pipeline_stage_04 = DataTranerTrainingPipeline()
    pipeline_stage_04.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
