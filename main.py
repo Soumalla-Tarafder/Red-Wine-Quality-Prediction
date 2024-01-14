3bbfc6b8c1a4d2649fc69b7e0436378b49207d8f
soumalla <soumallatarafder@gmail.com>
2024-01-14 23:04:33 +0530

    data_validation stager completed

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from red_wine_quality_prediction.config.configuration import ConfigurationManager
-from red_wine_quality_prediction.components.data_validation import DataValiadtion
+from red_wine_quality_prediction.components.data_transformation import DataTransformation
 from red_wine_quality_prediction import logger
 
 
-STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Transformation stage"
 
-class DataValidationTrainingPipeline:
+class DataTransformationTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_validation_config = config.get_data_validation_config()
-        data_validation = DataValiadtion(config=data_validation_config)
-        data_validation.validate_all_columns()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
 
 
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
