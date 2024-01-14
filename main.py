ea01578dba9d1642b9d4c85d3772cddc91ac9a2d
soumalla <soumallatarafder@gmail.com>
2024-01-14 19:28:22 +0530

    Data Ingestion stage Complete

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from red_wine_quality_prediction import logger
+from red_wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("working perfectly")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
