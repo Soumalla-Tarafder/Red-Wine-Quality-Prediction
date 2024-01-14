2c208115dd03e5e67aa48bff06465a45be348499
soumalla <soumallatarafder@gmail.com>
2024-01-14 22:44:27 +0530

    data validation stage all updated

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from red_wine_quality_prediction.config.configuration import ConfigurationManager
+from red_wine_quality_prediction.components.data_validation import DataValiadtion
 from red_wine_quality_prediction import logger
-from red_wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
-STAGE_NAME = "Data Ingestion stage"
 
 
-try:
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__ == '__main__':
+    try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = DataValidationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
+    except Exception as e:
         logger.exception(e)
         raise e
