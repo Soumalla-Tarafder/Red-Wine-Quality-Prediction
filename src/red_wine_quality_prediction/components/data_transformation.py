8679eb388ab528cd05964a45366ec0c1224660d2
soumalla <soumallatarafder@gmail.com>
2024-01-16 00:02:45 +0530

    final update for prediction this two files

--- src/red_wine_quality_prediction/components/data_transformation.py
+++ src/red_wine_quality_prediction/components/data_transformation.py
@@ -21,7 +21,7 @@ class DataTransformation:
 
     def train_test_spliting(self):
         data = pd.read_csv(self.config.data_path)
-
+        del data[data. columns[0]]
         # Split the data into training and test sets. (0.75, 0.25) split.
         train, test = train_test_split(data)
 
